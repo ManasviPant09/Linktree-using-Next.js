b1b1ea87c8e25ec9f6f760c759a34a812ee30c5b
Manasvi Pant <manasvipant1106@gmail.com>
2023-04-04 16:16:49 +0530

    Initial Commit

---
+++ data.js
@@ -0,0 +1,31 @@
+import { Profile } from "./assets";
+export const data = [
+    {
+        name: "Manasvi Pant",
+        icon: Profile,
+        links : [
+            {
+                title: "Portfolio Website",
+                url: "https://stately-sprite-a1bd0e.netlify.app"
+            },
+            {
+                title: "LinkedIn",
+                url: "https://www.linkedin.com/in/manasvi-pant-79a4071b3"
+            },
+            {
+                title: "Github",
+                url: "https://github.com/ManasviPant09"
+            }
+        ],
+        socials : [
+            {
+                title: "Medium",
+                url : "https://medium.com/@manasvipant1106"
+            },
+            {
+                title: "Instagram",
+                url : "https://www.instagram.com/_manasvipant/?igshid=YmMyMTA2M2Y="
+            },
+        ]
+    }
+]
